f83fd98fefd8b79d9c9027b2f453a49321519728
Mark Skinner <markskinner850@gmail.com>
2018-09-01 17:14:50 -0400

    Add latitude and longitude functionality

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: undefined,
+      longitude: undefined
+    }
+    this.getLocation = this.getLocation.bind(this)
+  }
+
+  getLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        })
+      });
+    } else {
+      console.log('No GeoLocation');
+    }
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.getLocation}>Get Location</button>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
