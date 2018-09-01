c603b164cc4b73434ce1c7ff26dd17a211495579
Mark Skinner <markskinner850@gmail.com>
2018-09-01 17:12:24 -0400

    Add Form component to app

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = props => (
+  <form onSubmit={props.getWeather}>
+    <input type='text' name='city' placeholder='Enter name of city here...' />
+    <input type='text' name='country' placeholder='Enter two letter country code' />
+    <button>Get Weather</button>
+  </form>
+);
+
+export default Form;
