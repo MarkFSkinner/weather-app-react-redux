edd63a6a2eb157d959b7c57653c3a4738c18252d
Mark Skinner <markskinner850@gmail.com>
2018-09-02 17:39:54 -0400

    Change formating for displayed weather data

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Weather = props => (
   <div>
-    { props.city && <p>City: {props.city}</p> }
-    { props.country && <p>Country: {props.country}</p> }
-    { props.temperature && props.unit && <p>Temperature: {props.temperature} {props.unit}</p> }
-    { props.humidity && <p>Humidity: {props.humidity}</p> }
-    { props.description && <p>Description: {props.description}</p> }
-    { props.icon && <p>Icon: <img src={props.icon} alt='Weather Icon'/></p> }
+    { props.city && props.country && <p>{props.city}, {props.country}</p> }
+    { props.temperature && props.unit && <p>{props.temperature} {props.unit}</p> }
+    { props.humidity && <p>Humidity: {props.humidity}%</p> }
+    { props.description && <p>{props.description}</p> }
+    { props.icon && <p><img src={props.icon} alt='Weather Icon'/></p> }
     { props.error && <p>Error: {props.error}</p> }
   </div>
 )
