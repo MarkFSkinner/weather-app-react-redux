a5b3faa5bc087b3ed161c20bccbb5b8769692be5
Mark Skinner <markskinner850@gmail.com>
2018-09-03 14:33:31 -0400

    Add css styling to Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const Weather = props => (
-  <div>
-    { props.city && props.country && <p>{props.city}, {props.country}</p> }
-    { props.temperature && props.unit && <p>{props.temperature} {props.unit}</p> }
-    { props.humidity && <p>Humidity: {props.humidity}%</p> }
-    { props.description && <p>{props.description}</p> }
-    { props.icon && <p><img src={props.icon} alt='Weather Icon'/></p> }
-    { props.error && <p>Error: {props.error}</p> }
+  <div id='weather_info'>
+    { props.city && props.country && <p id='weather__location'>{props.city}, {props.country}</p> }
+    { props.temperature && props.unit && <p id='weather__temperature'>{props.temperature} {props.unit}</p> }
+    { props.icon && <img id='weather__icon' src={props.icon} alt='Weather Icon'/> }
+    { props.description && <p id='weather__description'>{props.description}</p> }
+    { props.humidity && <p id='weather__humidity'>Humidity: {props.humidity}%</p> }
+    { props.error && <p id='weather__error'> Error: {props.error}</p> }
   </div>
 )
 
