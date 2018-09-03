e23f487e357764d175160a25a7c45ac0c8a0174b
Mark Skinner <markskinner850@gmail.com>
2018-09-03 16:45:38 -0400

    Add css style to app
    Mainly margin and padding updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ const Form = props => (
   <form onSubmit={props.getWeather}>
     <input type='text' name='city' id='city' placeholder='Enter name of city here...' />
     <input type='text' name='country' id='country' placeholder='Enter two letter country code' />
-    <button className='btn btn-primary'>Get Weather</button>
+    <button className='btn btn-primary btn_weather'>Get Weather</button>
   </form>
 );
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Title = () => (
-  <div>
+  <div id='weather_title'>
     <h1>Weather Finder</h1>
   </div>
 );
