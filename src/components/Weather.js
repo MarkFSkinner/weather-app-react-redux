7355cf2cf66410dc28597c0a9bb1fa33276a7335
Mark Skinner <markskinner850@gmail.com>
2018-09-03 16:18:52 -0400

    Add icons for humidity and wind

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,8 +6,8 @@ const Weather = props => (
     { props.icon && <img id='weather__icon' src={props.icon} alt='Weather Icon'/> }
     { props.temperature && props.unit && <p id='weather__temperature'>{props.temperature} {props.unit}</p> }
     { props.description && <p id='weather__description'>{props.description}</p> }
-    { props.humidity && <p id='weather__humidity'>Humidity: {props.humidity}%</p> }
-    { props.wind && props.direction && <p id='weather__wind'>Wind: {props.direction} {props.wind} m/s</p> }
+    { props.humidity && <p id='weather__humidity'><img src='https://cdn.onlinewebfonts.com/svg/img_493336.png'/> {props.humidity}%</p> }
+    { props.wind && props.direction && <p id='weather__wind'><img src='https://image.flaticon.com/icons/svg/56/56086.svg'/> {props.direction} {props.wind} m/s</p> }
     { props.error && <p id='weather__error'> Error: {props.error}</p> }
   </div>
 )
