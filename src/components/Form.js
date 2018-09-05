72e4c9d787143bf12957346aa187377c1663564e
Mark Skinner <markskinner850@gmail.com>
2018-09-05 19:24:08 -0400

    Add bootstrap classes to Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Form = props => (
-  <form onSubmit={props.getWeather}>
-    <input type='text' name='city' id='city' placeholder='Enter name of city here...' />
-    <input type='text' name='country' id='country' placeholder='Enter two letter country code' />
+  <form className='form-inline justify-content-center' onSubmit={props.getWeather}>
+    <div className='form-group'>
+      <label className="sr-only" for="city">City</label>
+      <input type='text' className='form-control' name='city' id='city' placeholder='Enter name of city here...' />
+    </div>
+    <div className='form-group'>
+      <label className="sr-only" for="country">Country</label>
+      <input type='text' className='form-control' name='country' id='country' placeholder='Enter two letter country code' />
+    </div>
     <button className='btn btn-primary btn_weather'>Get Weather</button>
   </form>
 );
