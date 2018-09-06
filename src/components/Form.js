9496bee039ae560f7c26a157fba4df9c124f4147
Mark Skinner <markskinner850@gmail.com>
2018-09-06 15:39:43 -0400

    Make Form component responsive for mobile

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Form = props => (
   <form className='form-inline justify-content-center' onSubmit={props.getWeather}>
-    <div className='form-group'>
+    <div className='form-group my-form'>
       <label className="sr-only" for="city">City</label>
-      <input type='text' className='form-control' name='city' id='city' placeholder='Enter name of city here...' />
+      <input type='text' className='form-control my-input' name='city' id='city' placeholder='Enter name of city here...' />
     </div>
-    <div className='form-group'>
+    <div className='form-group my-form'>
       <label className="sr-only" for="country">Country</label>
-      <input type='text' className='form-control' name='country' id='country' placeholder='Enter two letter country code' />
+      <input type='text' className='form-control my-input' name='country' id='country' placeholder='Enter two letter country code' />
     </div>
     <button className='btn btn-primary btn_weather'>Get Weather</button>
   </form>
