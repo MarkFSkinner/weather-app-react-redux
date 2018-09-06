6d4665b1f7feadec4603b0563695569c464de3dc
Mark Skinner <markskinner850@gmail.com>
2018-09-06 16:35:03 -0400

    Change form input placeholder text

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,11 +4,11 @@ const Form = props => (
   <form className='form-inline justify-content-center' onSubmit={props.getWeather}>
     <div className='form-group my-form'>
       <label className="sr-only" for="city">City</label>
-      <input type='text' className='form-control my-input' name='city' id='city' placeholder='Enter name of city here...' />
+      <input type='text' className='form-control my-input' name='city' id='city' placeholder='City name...' />
     </div>
     <div className='form-group my-form'>
       <label className="sr-only" for="country">Country</label>
-      <input type='text' className='form-control my-input' name='country' id='country' placeholder='Enter two letter country code' />
+      <input type='text' className='form-control my-input' name='country' id='country' placeholder='Two letter country code...' />
     </div>
     <button className='btn btn-primary btn_weather'>Get Weather</button>
   </form>
