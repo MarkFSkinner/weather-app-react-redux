b6f9bbb595ad9c59445ddd04e0682d4c005fb16e
Mark Skinner <markskinner850@gmail.com>
2018-09-08 17:14:29 -0400

    Change p tags to div in Weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Weather = props => (
   <div id='weather_info'>
-    { props.city && props.country && <p id='weather__location'>{props.city}, {props.country}</p> }
+    { props.city && props.country && <div id='weather__location'>{props.city}, {props.country}</div> }
     { props.icon && <img id='weather__icon' src={props.icon} alt='Weather Icon'/> }
-    { props.temperature && props.unit && <p id='weather__temperature'>{props.temperature} {props.unit}</p> }
-    { props.description && <p id='weather__description'>{props.description}</p> }
-    { props.humidity && <p id='weather__humidity'><img src='https://cdn.onlinewebfonts.com/svg/img_493336.png'/> {props.humidity}%</p> }
-    { props.wind && <p id='weather__wind'><img src='https://image.flaticon.com/icons/svg/56/56086.svg'/> {props.direction} {props.wind} m/s</p> }
-    { props.message && <p id='weather__message'>*Error: {props.message}*</p> }
+    { props.temperature && props.unit && <div id='weather__temperature'>{props.temperature}<span id='weather__unit'>{props.unit}</span></div> }
+    { props.description && <div id='weather__description'>{props.description}</div> }
+    { props.humidity && <div id='weather__humidity'><img src='https://cdn.onlinewebfonts.com/svg/img_493336.png'/> {props.humidity}%</div> }
+    { props.wind && <div id='weather__wind'><img src='https://image.flaticon.com/icons/svg/56/56086.svg'/> {props.direction} {props.wind} m/s</div> }
+    { props.message && <div id='weather__message'>*Error: {props.message}*</div> }
   </div>
 )
 
